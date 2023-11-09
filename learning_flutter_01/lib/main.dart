117ff1a85cd144659df4c2d6900ae89f751d90b9
Corey Morehead <cmoreheadno2@gmail.com>
2023-11-09 16:26:16 -0500

    Section3.26
    Added an image, scaffold, centered, fixed const

--- learning_flutter_01/lib/main.dart
+++ learning_flutter_01/lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+          ),
+        ),
       ),
     ),
   );
