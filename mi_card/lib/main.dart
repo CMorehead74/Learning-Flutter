294f13da968bbb88dc94094684e0cb4cc6504ea5
Corey Morehead <cmoreheadno2@gmail.com>
2023-11-17 13:11:24 -0500

    completed homework

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -3,36 +3,38 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               Container(
-                height: 100.0,
-                width: double.infinity,
-                color: Colors.white,
+                width: 100.0,
+                height: double.infinity,
+                color: Colors.red,
                 child: Text('Container 1'),
               ),
               SizedBox(
-                height: 20.0
+                width: 10.0
               ),
               Container(
                 height: 100.0,
-                width: double.infinity,
-              color: Colors.blueGrey,
+                width: 100.0,
+              color: Colors.yellow,
               child: Text('Container 2'),
               ),
+              SizedBox(
+                width: 10.0
+              ),
               Container(
-                height: 100.0,
-                width: double.infinity,
-                color: Colors.red,
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.green,
                 child: Text('Container 3'),
               )
             ],
