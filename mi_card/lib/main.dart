d6da1780a290086653dd4137d571caeeb7bad5f5
Corey Morehead <cmoreheadno2@gmail.com>
2023-11-17 14:44:46 -0500

    updated yaml with image

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -10,33 +10,12 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 100.0,
-                height: double.infinity,
-                color: Colors.red,
-                child: Text('Container 1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
               ),
-              SizedBox(
-                width: 10.0
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-              color: Colors.yellow,
-              child: Text('Container 2'),
-              ),
-              SizedBox(
-                width: 10.0
-              ),
-              Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.green,
-                child: Text('Container 3'),
-              )
             ],
           ),
         ),
