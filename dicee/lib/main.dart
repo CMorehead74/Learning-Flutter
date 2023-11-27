3176454287c46684007cca1c08dfa26f78a8d5a4
Corey Morehead <cmoreheadno2@gmail.com>
2023-11-27 13:02:44 -0500

    Update main.dart
    added stateful dice

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,31 +16,41 @@ void main() {
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
 
-class DicePage extends StatelessWidget {
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 1;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: GestureDetector(onTap:() {
-                  print('Leftbutton go tapped');
-                  },
-                  child: Image.asset('images/dice$leftDiceNumber.png'),
-                ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GestureDetector(onTap:() {
+                setState((){
+                  leftDiceNumber = 5;
+                });
+              },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
               ),
-              ),
+            ),
+          ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: GestureDetector(onTap: () {
-                print('Rightbutton go tapped');
-                },
-              child: Image.asset('images/dice1.png')
+                setState((){
+                  rightDiceNumber = 5;
+                });
+              },
+                  child: Image.asset('images/dice$rightDiceNumber.png')
               ),
             ),
           ),
