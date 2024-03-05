231328c6aeb0d230e45d5e8c8221371b8e48858f
Corey Morehead <cmoreheadno2@gmail.com>
2024-03-05 11:35:13 -0500

    Update main.dart
    added list

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -25,6 +25,30 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -53,6 +77,11 @@ class _QuizPageState extends State<QuizPage> {
             child: GestureDetector(
               onTap: () {
                 //The user picked true.
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(Icons.check, color: Colors.green),
+                  );
+                });
               },
               child: Container(
               //textColor: Colors.white,
@@ -90,7 +119,10 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
+
       ],
     );
   }
